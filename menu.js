5a951908e7a79ad11129b1b693bdf4aea56ded92
RenanNbr <128818511+RenanNbr@users.noreply.github.com>
2023-10-06 11:53:34 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,21 @@
+function escolher(){
+    var opcao = prompt("escolha uma opção de 1 a 4")
+
+    switch(Number(opcao)){
+        case 1:
+            alert("Você escolheu Banana")
+            break;
+
+            case 2:
+                alert(" Você escolheu Laranja")
+                break;
+
+                case 3:
+                    alert(" Você escolheu Maça")
+                    break;
+
+                    case 4:
+                        alert(" Você escolheu Uva")
+                        break;
+    }
+}
